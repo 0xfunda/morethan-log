ca670ff038d98672e84e85491078ad1688ece5df
Reza <76862881+0xfunda@users.noreply.github.com>
2023-11-03 21:28:28 +0330

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "0xflutter",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Alpha Hunter",
+    bio: "Research and Hunting Early Web3 projects",
+    email: "me@flutter.xyz",
+    linkedin: "reza-rezaei-461b041b2",
+    github: "0xflutter",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Early Projects",
   },
 
   // CONFIG configration (required)
